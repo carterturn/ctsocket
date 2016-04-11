2a75afa5ee0017c439c41197022861baed76c206
Carter Turnbaugh <carterturn@tutanota.de>
2016-04-11 11:31:52 -0700

    Merge of tests

--- tclient.cpp
+++ tclient.cpp
@@ -23,25 +23,13 @@ using namespace std;
 
 int main(){
 
-<<<<<<< HEAD
-		ctclient client;
-=======
-	ctclientsecure client("passwordpassword");
->>>>>>> master
+	ctclient client;
 
 	client.create("127.0.0.1", 5552);
 
 	client.c_write("Testing 1 2 3. Testing 4 5 6. using namespace std;    Copyright 2016 Carter Turnbaugh  This file is part of Terca C++ Sockets.  Terca C++ Sockets is free software: you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by  the Free Software Foundation, either version 3 of the License, or  (at your option) any later version.  Terca C++ Sockets is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License for more details.  You should have received a copy of the GNU General Public License  along with Terca C++ Sockets.  If not, see <http://www.gnu.org/licenses/>.");
 	cout << client.c_read() << "\n";
 
-<<<<<<< HEAD
-		client.c_close();
-=======
-	client.s_write("#include <iostream> int main(int argc, char* argv[]){}    Copyright 2016 Carter Turnbaugh  This file is part of Terca C++ Sockets.  Terca C++ Sockets is free software: you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by  the Free Software Foundation, either version 3 of the License, or  (at your option) any later version.  Terca C++ Sockets is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License for more details.  You should have received a copy of the GNU General Public License  along with Terca C++ Sockets.  If not, see <http://www.gnu.org/licenses/>.");
-	cout << client.s_read() << "\n";
-
 	client.c_close();
->>>>>>> master
-	
 	return 0;
 }
