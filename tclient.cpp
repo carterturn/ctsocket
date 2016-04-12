4de33bfa29de9c0bed180bc122d0f5dec84fdb02
Carter Turnbaugh <carterturn@tutanota.de>
2016-04-11 18:03:17 -0700

    More rigorous testing

--- tclient.cpp
+++ tclient.cpp
@@ -25,9 +25,9 @@ int main(){
 
 	ctclientsecure client("passwordpassword");
 
-	client.create("127.0.0.1", 5553);
+	client.create("127.0.0.1", 5552);
 
-	client.c_write("Testing 1 2 3. Testing 4 5 6. using namespace std;    Copyright 2016 Carter Turnbaugh  This file is part of Terca C++ Sockets.  Terca C++ Sockets is free software: you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by  the Free Software Foundation, either version 3 of the License, or  (at your option) any later version.  Terca C++ Sockets is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License for more details.  You should have received a copy of the GNU General Public License  along with Terca C++ Sockets.  If not, see <http://www.gnu.org/licenses/>.");
+	client.c_write("Testing 1 2 3. Testing 4 5 6. using namespace std;    Copyright 2016 Carter Turnbaugh  This file is part of Terca C++ Sockets.  Terca C++ Sockets is free software: you can redistribute it and/or modify  it under the terms of the GNU General Public License");
 	cout << client.c_read() << "\n";
 
 	client.s_write("#include <iostream> int main(int argc, char* argv[]){}    Copyright 2016 Carter Turnbaugh  This file is part of Terca C++ Sockets.  Terca C++ Sockets is free software: you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by  the Free Software Foundation, either version 3 of the License, or  (at your option) any later version.  Terca C++ Sockets is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License for more details.  You should have received a copy of the GNU General Public License  along with Terca C++ Sockets.  If not, see <http://www.gnu.org/licenses/>.");
